6c6055f9d487d5746b62b1de93512e8afd695273
hp <hp@LAPTOP-0VH39274>
2024-07-22 08:43:25 +0530

    Add project files.

---
+++ E-Commerce_Housing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Commerce_Housing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
